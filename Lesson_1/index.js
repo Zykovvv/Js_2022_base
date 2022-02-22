6ddc15498bd6020927e0fdc939b22e36db7bbd8e
Maxim Zykov <zykovworks@yandex.ru>
2022-02-22 09:38:00 +0300

    Доб. Дз с переменными

---
+++ Lesson_1/index.js
@@ -0,0 +1,15 @@
+console.log ('Hello Javascript');
+
+const myName = 'Maxim';
+
+console.log (myName);
+
+const nameOurPlanet = 'Earth'; //Переменная для “названия нашей планеты”
+const currentTimeUser = '12:00'; //Переменная для “текущее время пользователя”
+const quantityArticles = 100; //Переменная которая показывает “количества статей”
+const cashPaymentOrNot = 'Cash'; //Переменная которая показывает “это оплата наличными деньгами или нет”
+
+//Три переменные для хранения Ф.И.О
+const userName = 'Maxim';
+const userSurname = 'Zykov';
+const userPatronymic = 'Pavlovich';
