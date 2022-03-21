ae59104cfadd7fae133d6198b3b9a3834dd11c25
Maxim Zykov <zykovworks@yandex.ru>
2022-03-21 11:35:42 +0300

    Добавил второе дз

--- Lesson_2/Homework/index2.js
+++ Lesson_2/Homework/index2.js
@@ -1,30 +1,21 @@
-// Практический материал к презентации №2
-
-// "кот" > "код" 
-let mmm = "кот" > "код" //true 
-console.log (mmm)
-
-// "2" + 2 * "2"
-let aaa = "2" + 2 * "2" //24
-console.log (aaa)
-
-// undefined == null
-console.log (undefined == null)
-// undefined != null 
-// null == 0 
-// 2 > "3" 
-// null - false + true 
-// 1 / "l"
-// "2" * "3" 
-// 4 + 5 + "O" 
-// "l" + 4 + 5 
-// "4" - 2 
-// "4" - "4x" 
-// “23” == 23
-// null == false 
-// " -4 "/ 0 + 1 
-// null + 1 
-// undefined + null
-// 1 === “1”
-// “2” > 10
-// NaN == undefined
+console.log ("кот"> "код"); //true
+console.log ("2" + 2 * "2"); //24
+console.log (undefined == null); //true
+console.log (undefined != null); //false
+console.log (null == 0); //false
+console.log (2 > "3"); //false
+console.log (null - false + true); //1
+console.log (1 / "l"); //NaN
+console.log ("2" * "3"); //6
+console.log (4 + 5 + "O"); //90
+console.log ("l" + 4 + 5); //l45 
+console.log ("4" - 2); //2
+console.log ("4" - "4x"); //NaN
+console.log ("23" == 23); //true
+console.log (null == false); //false
+console.log (" -4 "/ 0 + 1); //-infinity
+console.log (null + 1); //1
+console.log (undefined + null);//NaN
+console.log (1 === "1"); //false
+console.log ("2" > 10); //false
+console.log (NaN == undefined); //false
