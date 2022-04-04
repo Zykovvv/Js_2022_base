1706f33e8a42df681b47201d544bfc0a5370ff12
Maxim Zykov <zykovworks@yandex.ru>
2022-04-04 18:35:48 +0300

    Добавил дз по уроку №4

---
+++ Lesson_4/Homework/index.js
@@ -0,0 +1,184 @@
+// Задачи из learnjavascript
+// IF, ELSE, ?
+
+// const titleOfficial = "ECMAScript";
+// let vvod = prompt("Введите оффициальое название языка JavaScript" , "");
+
+// if (vvod === titleOfficial){
+//     console.log("Верно!");
+// } else {
+//     console.log("Не знаете? ECMAScript");
+// }
+
+// let value = prompt("Введите число", 0);
+
+// if (value > 0){
+//     console.log("Значение больше нуля")
+// }else if (value <= -1){
+//     console.log( "Значение меньше нуля")
+// }else{
+//     console.log ("Значение равно нулю")
+// }
+
+
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+
+// let result = (a+b < 4)? "Malo" : "Mnogo";
+
+// &&, ||, !
+
+// let age = 90;
+
+// if (age >=14 && age <=90){
+//     console.log ("Условие выполняется");
+// }else{
+//     console.log("Ошибочка");
+// }
+
+// let age = 100;
+
+// if (!(age >=14 && age <=90)){
+//     console.log ("Условие выполняется");
+// }else{
+//     console.log("Ошибочка");
+// }
+
+// let value = prompt('Кто там?' , );
+// const userName = 'Admin';
+
+// if(value !=== userName){
+//     console.log("Я вас не знать")
+
+
+// if (value === userName){
+//     console.log('Говори пароль')
+// }else if (value === "" || value === null){
+//     console.log('Отменено')
+//     }
+// }
+
+
+// let i = 3;
+
+// while (i) {
+//   console.log( i-- );
+// }
+
+
+// ДОМАШНЕЕ ЗАДАНИЕ ПО УРОКУ 4
+// Работа с if-else
+
+// let a = -3;
+
+// if (a===0){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+// let a = 1;
+
+// if (a > 0){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+
+// let a = 1;
+
+// if (a < 0){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+// let a = 1;
+
+// if (a >= 0){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+
+// let a = 1;
+
+// if (a <= 0){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+// let a = 1;
+
+// if (a != 0){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+
+// Работа с логическими переменными
+
+
+// let a = "тест";
+
+// if (a === "Тест"){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+// let a = 1;
+
+// if (a === "1"){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+
+// let test = false;
+
+// if (test === true){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+// let test = (test===true)? true:false;
+
+// let test = true;
+
+// if (test != true){
+//     console.log("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+
+
+// Работа с && (и) и || (или)
+
+// let a = 0;
+
+// if(a > 0 && a < 5){
+//     console.log ("Верно")
+// }else{
+//     console.log("Неверно")
+// }
+
+// let a = 0;
+
+// if(a > 0 && a < 5){
+//     console.log ("Верно")
+// }else{
+//     console.log("Неверно")
+// }
